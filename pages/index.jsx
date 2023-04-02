fcdf89a14d6ff82f3ac7b80160a240934d201244
Eric Galvão <ericagoficial@gmail.com>
2023-04-02 16:10:55 -0300

    cirado componentes CreatePost

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,24 @@
+import styled from 'styled-components'
+
 import Navbar from "../src/componentes/layout/Navbar"
+import Container from '../src/componentes/layout/Container'
+import CreatePost from '../src/componentes/cards/CreatePost'
+
+const Content = styled.div`
+  margin: 50px 0;
+`
+
 
 function HomePage () {
   
   return (
     <>
-      <div>
       <Navbar />
-        SejaDev
-      </div>
+      <Content>
+        <Container>
+          <CreatePost />
+        </Container>
+      </Content>
     </>
   )
 }
---
+++ src/componentes/cards/CreatePost.jsx
@@ -0,0 +1,52 @@
+import styled from "styled-components"
+
+import H4 from "../tipografia/H4"
+import TextArea from "../inputs/TextArea"
+import Button from "../inputs/Button"
+
+const PostContainer = styled.div`
+    background-color: white;
+    padding: 20px 40px;
+
+    @media (max-width: 500px) {
+        padding: 20px;
+    }
+`
+const Title = styled.div`
+    font-weight: bold;
+    text-align: center;
+`
+const TextContainer = styled.div`
+    margin: 20px 0;
+`
+
+const BottomContainer = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 5px;
+
+    @media (max-width: 500px) {
+        flex-direction: column-reverse;
+    }
+`
+
+const BottomText = styled.p`
+    flex: 1;
+`
+
+function CreatePost () {
+    return (
+        <PostContainer>
+            <H4><Title>No que você está pensando? @nick</Title></H4>
+            <TextContainer>
+                <TextArea placeholder="Digite sua mensagem" rows="4"/>
+            </TextContainer>
+            <BottomContainer>
+                <BottomText>A sua mensagem será pública.</BottomText>
+                <Button>Enviar Mensagem</Button> 
+            </BottomContainer>
+        </PostContainer>
+    )
+}
+
+export default CreatePost
