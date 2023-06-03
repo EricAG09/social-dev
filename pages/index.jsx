96580a11a74c6e92a300b0df4fd1168d39a0430c
Eric Galvão <ericagoficial@gmail.com>
2023-06-03 09:27:51 -0300

    alterado busca de posts usanddo a lib swr

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import useSWR from 'swr'
 import styled from 'styled-components'
 import {withIronSessionSsr} from 'iron-session/next'
 import {ironConfig} from '../lib/middlewares/ironSession'
@@ -36,17 +37,11 @@ const PostContainer = styled.div`
   margin-top: 20px;
 `
 
+  const fetcher = url => axios.get(url).then(res => res.data)
+
 function HomePage ({user}) {
-  const  [data, setData] = useState([])
 
-  const handlePosts = async () => {
-    const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/post`)
-    setData(response.data)
-  }
-
-  useEffect(()=> {
-    handlePosts()
-  }, [])
+  const {data} = useSWR(`${process.env.NEXT_PUBLIC_API_URL}/api/post`, fetcher)
 
   return (
     <>
@@ -62,7 +57,7 @@ function HomePage ({user}) {
           </RefreshPostsContainer>
           <PostContainer>
             {
-              data.map(post => 
+              data?.map(post => 
                 <Post 
                   key={post._id}
                   text={post.text}
